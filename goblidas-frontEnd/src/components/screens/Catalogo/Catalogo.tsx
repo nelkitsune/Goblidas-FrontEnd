554ade515e86a596cc2bbd3bd8a659e27c26f9f6
RamiroFerrari <ramiroferrariadrian@gmail.com>
2025-05-12 14:00:10 -0300

    avance cartegoria

--- goblidas-frontEnd/src/components/screens/Catalogo/Catalogo.tsx
+++ goblidas-frontEnd/src/components/screens/Catalogo/Catalogo.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { SideCatalogo } from '../../ui/SideCatalogo/SideCatalogo'
+import './CatalogoEstilo.css'
+
 
 export const Catalogo = () => {
+
+    const [sideCatalogo, setSideCatalogo] = useState(false)
+
+    const handleOpenSideCatalogo = () => {
+        console.log(sideCatalogo)
+        setSideCatalogo(!sideCatalogo)
+    }
     return (
         <>
             <div className="Catalogo">
+                <button onClick={handleOpenSideCatalogo} className="btnFiltros">
+                    Categorias
+                </button>
+                {sideCatalogo && <SideCatalogo />}
                 <h1>Catalogo</h1>
                 <p>Esta es la pantalla de catalogo</p>
             </div>
         </>
     )
 }
+
